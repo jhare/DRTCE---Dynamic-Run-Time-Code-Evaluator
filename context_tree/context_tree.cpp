122b7b7fddec1072a3ea455fca47aee7f75cbc1d
jhare <joseph.hare@gmail.com>
2006-10-11 20:17:22 +0000

    I made out the basic structure of the context tree. This included creating an enumerated type, a struct for the tree nodes, as well as the tree class itself. The nodes are set up to be n-ary, in that they just have a basic ** for the child ptr. - Joe

---
+++ context_tree/context_tree.cpp
@@ -0,0 +1,16 @@
+#include "context_tree.h"
+
+drtce::context_tree::context_tree()
+{
+
+}
+
+drtce::context_tree::~context_tree()
+{
+
+}
+
+void drtce::context_tree::add_node( drtce::i_context type, std::string context )
+{
+
+}
--- context_tree/context_tree.h
+++ context_tree/context_tree.h
@@ -1,4 +1,32 @@
 #ifndef __CONTEXT_TREE
 #define __CONTEXT_TREE
 
+namespace drtce
+{
+	enum i_context { c_loop, c_func, c_block };
+
+	struct ctree_node
+	{
+		unsigned int _cid;
+		std::vector< std::string > dependents;
+
+		ctree_node* parent;
+		ctree_node** children;
+	};
+
+	class context_tree
+	{
+
+		public:
+
+			context_tree();
+			~context_tree();
+
+			void add_node( i_context type, std::string context = "<null>" );	
+
+		private:
+
+			ctree_node* m_root;
+	};
+}
 #endif
